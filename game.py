a75aba1fcc0297bc1ba70a56b434ca1bae5766f1
Jesse Na <code@yhnl.mozmail.com>
2026-01-30 13:25:09 -0500

    add a second plot

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ WIDTH, HEIGHT = 60, 40
 # Create random initial state
 grid = np.random.randint(0, 2, size=(HEIGHT, WIDTH))
 
+
 def count_live_neighbors(x, y):
     # Walk students through this function
     total = 0
@@ -20,6 +21,7 @@ def count_live_neighbors(x, y):
             if 0 <= nx < HEIGHT and 0 <= ny < WIDTH:
                 total += 1 if grid[nx, ny] > 0 else 0
     return total
+
 
 def update(frame):
     global grid
@@ -42,16 +44,25 @@ def update(frame):
                     new_grid[i, j] = 1  # born
 
     grid = new_grid
-    mat.set_data(grid)
-    return [mat]
+    mat1.set_data(grid)
+    mat2.set_data(grid > 0)
+    return [mat1]
+
 
 # Plot setup
-fig, ax = plt.subplots()
-mat = ax.imshow(grid, cmap="plasma", interpolation="nearest", vmin=0, vmax=100)
-ax.set_xticks([])
-ax.set_yticks([])
+fig, ax = plt.subplots(1, 2, figsize=(10, 6))
+mat1 = ax[0].imshow(grid, cmap="plasma",
+                    interpolation="nearest", vmin=0, vmax=100)
+ax[0].set_xticks([])
+ax[0].set_yticks([])
 
-plt.colorbar(mat, label="Cell Age")
+plt.colorbar(mat1, label="Cell Age", ax=ax[0])
+mat2 = ax[1].imshow(grid > 0, cmap="gray",
+                    interpolation="nearest")
+ax[1].set_xticks([])
+ax[1].set_yticks([])
+plt.colorbar(mat2, label="Alive/Dead", ax=ax[1])
+
 ani = FuncAnimation(fig, update, interval=100)
 
 plt.show()
